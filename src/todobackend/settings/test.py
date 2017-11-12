6999873dc6b78db1ddbf9c110c4b79367dc6bd86
carello <cp2carello@gmail.com>
2017-11-12 08:55:22 -0600

    2nd commmit

--- src/todobackend/settings/test.py
+++ src/todobackend/settings/test.py
@@ -23,7 +23,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('MYSQL_DATABASE', 'todobackend'),
         'USER': os.environ.get('MYSQL_USER', 'todo'),
-        'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'password'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD', '9letmein'),
         'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
         'PORT': os.environ.get('MYSQL_PORT', '3306'),
     }
